377a680eeacd703a5b4ac716d96f98f381e6d6e0
Dave Casinginan <davecasinginan@gmail.com>
2025-05-09 13:32:01 +0800

    program pseudocode

---
+++ quiz_program.py
@@ -0,0 +1,7 @@
+#import random for randomizing questions later
+#load the file from the quiz creator
+#split the questions into blocks
+#Extract the questions, options, and answers
+#randomize the questions
+#display and prompt users to answer
+#check answer
