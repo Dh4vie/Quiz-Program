0e41031d1e485aa64cb0de03b98174a26fecd0af
Dave Casinginan <davecasinginan@gmail.com>
2025-05-09 13:51:15 +0800

    called file and split questions into blocks

--- quiz_program.py
+++ quiz_program.py
@@ -1,6 +1,15 @@
 #import random for randomizing questions later
+import random
+
 #load the file from the quiz creator
+filename = gathered_quiz_data.txt
+
+with open(filename, 'r') as file:
+    content = file.read()
+
 #split the questions into blocks
+question_blocks = content.strip().split('-' * 40)
+
 #Extract the questions, options, and answers
 #randomize the questions
 #display and prompt users to answer
